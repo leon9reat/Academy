9e7c8919424bcafb6f799f7454704d30b89ccbcf
Benk-Office <mail.buat.forum@gmail.com>
2021-05-04 11:05:58 +0700

    ViewModel dalam Proyek Android Jetpack

--- app/src/main/java/com/medialink/academy/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/medialink/academy/ui/detail/DetailCourseActivity.kt
@@ -1,10 +1,19 @@
 package com.medialink.academy.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.medialink.academy.R
 import com.medialink.academy.databinding.ActivityDetailCourseBinding
 import com.medialink.academy.databinding.ContentDetailCourseBinding
+import com.medialink.academy.ui.CourseEntity
+import com.medialink.academy.ui.reader.CourseReaderActivity
+import com.medialink.academy.utils.DataDummy
 
 class DetailCourseActivity : AppCompatActivity() {
 
@@ -22,5 +31,53 @@ class DetailCourseActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val adapter = DetailCourseAdapter()
+        val extras = intent.extras
+        if (extras != null) {
+            val courseId = extras.getString(EXTRA_COURSE)
+            if (courseId != null) {
+                val modules = DataDummy.generateDummyModules(courseId)
+                adapter.setModules(modules)
+                for (course in DataDummy.generateDummyCourses()) {
+                    if (course.courseId == courseId) {
+                        populateCourse(course)
+                    }
+                }
+            }
+        }
+
+        with(detailContentBinding.rvModule) {
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(this@DetailCourseActivity)
+            setHasFixedSize(true)
+            this.adapter = adapter
+            val dividerItemDecoration =
+                DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+    }
+
+    private fun populateCourse(courseEntity: CourseEntity) {
+        with(detailContentBinding) {
+            textTitle.text = courseEntity.title
+            textDescription.text = courseEntity.description
+            textDate.text = resources.getString(R.string.deadline_date, courseEntity.deadline)
+        }
+
+        Glide.with(this)
+            .load(courseEntity.imagePath)
+            .transform(RoundedCorners(20))
+            .apply(
+                RequestOptions.placeholderOf(R.drawable.ic_loading)
+                    .error(R.drawable.ic_error)
+            )
+            .into(detailContentBinding.imagePoster)
+
+        detailContentBinding.btnStart.setOnClickListener {
+            val intent = Intent(this@DetailCourseActivity, CourseReaderActivity::class.java)
+            intent.putExtra(CourseReaderActivity.EXTRA_COURSE_ID, courseEntity.courseId)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/medialink/academy/ui/home/HomeActivity.kt
+++ app/src/main/java/com/medialink/academy/ui/home/HomeActivity.kt
@@ -3,10 +3,18 @@ package com.medialink.academy.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.medialink.academy.R
+import com.medialink.academy.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(activityHomeBinding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        activityHomeBinding.viewPager.adapter = sectionsPagerAdapter
+        activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
+
+        supportActionBar?.elevation = 0f
     }
 }
